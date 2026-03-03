5b86ff3153dc95eca102c99f8e2f02ee31935980
dbrogen <daniel.brogen1@marist.edu>
2026-03-03 18:13:09 -0500

    Puzzle State Transition Integrated
    Overview:
    - Integrated the puzzle made by @mannykun with the game state manager made by @cassdaw
    
    Details:
    - Added a function to the game state manager called onPuzzleTrigger() function
         - This function allows for the transition in and out of the puzzle game state
    - Modified the InteractablePillar script to transition to the puzzle state using the new game state manager function

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractablePillar.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractablePillar.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class InteractablePillar : MonoBehaviour, IInteractable
 {
+
+    public GameStateManager GameStateManager;
+
     public void Interaction()
     {
         Debug.Log("Interacting");
-        SceneManager.LoadScene("TileMoveExperiment");
 
+        GameStateManager.onPuzzleTrigger();
     }
 }
