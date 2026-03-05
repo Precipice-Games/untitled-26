12da730845bdc54f0d09df14231867c07ae8481b
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-03-05 00:11:33 -0500

    Able to Snap Player to Tiles (WIP)
    Overview
    - Updated several movement-related scripts, including PlayerFixedMovement.cs.
    - Updated Ice_Island.unity.
    - This commit is related to #137.
    
    In-depth Details
    - Added additional logic that snaps the Player to the correct tile.
    - The only issue is that I'm struggling to update the coordinates (X,Z) for the Player in the context of grid coordinates.
         - Specifically what is used for any given tile in GridManager.cs.
    - Also set the puzzle UI in Puzzle1 to be disabled by default.
         - It is correctly enabled when we trigger a puzzle event, as explained in c1bd456.
    - If we can get the Player to have their own coordinates that correspond to the grid created in GridManager.cs, we will have a solid fixed movement system.

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Player/PlayerFixedMovement.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Player/PlayerFixedMovement.cs
@@ -2,8 +2,10 @@
 using UnityEditor.Build;
 #endif
 
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector3 = UnityEngine.Vector3;
 
 // This script is used to snap the Player in a fixed movement style during puzzle mode.
 // It is similar to the PlayerMovement.cs script, but it is used to snap the player to
@@ -30,6 +32,10 @@ public class PlayerFixedMovement : MonoBehaviour
     // The Player's X and Z coordinates on the grid.
     private int playerGridX;
     private int playerGridZ;
+    
+    // The new coordinates as a Vector3
+    Vector3 newCoords;
+    Vector3 newPosition;
 
    // Player's Rigidbody
     Rigidbody rb;
@@ -66,6 +72,7 @@ public class PlayerFixedMovement : MonoBehaviour
         gridManager = puzzleInfo.gridManager.GetComponent<GridManager>();
         startTile = puzzleInfo.startTile;
         endTile = puzzleInfo.endTile;
+        MoveToStartTile();
     }
     
     // The following methods listen to callback events from the Puzzle map from the
@@ -76,40 +83,54 @@ public class PlayerFixedMovement : MonoBehaviour
     public void MoveUp(InputAction.CallbackContext context)
     {
         // Ensures that the action is only performed once per key press.
-        if (context.performed)
-        {
-            Debug.Log("PlayerFixedMovement.cs >> MoveUp performed.");
-            TryToMovePlayer(0, 1);
-        }
+        if (!context.performed) return;
+        
+        Debug.Log("PlayerFixedMovement.cs >> MoveUp performed.");
+        TryToMovePlayer(0, 1);
     }
     
     public void MoveDown(InputAction.CallbackContext context)
     {
         // Ensures that the action is only performed once per key press.
-        if (context.performed)
-        {
-            Debug.Log("PlayerFixedMovement.cs >> MoveDown called.");
-            TryToMovePlayer(0, -1);
-        }
+        if (!context.performed) return;
+        
+        Debug.Log("PlayerFixedMovement.cs >> MoveDown called.");
+        TryToMovePlayer(0, -1);
     }
     
     public void MoveLeft(InputAction.CallbackContext context)
     {
         // Ensures that the action is only performed once per key press.
-        if (context.performed)
-        {
-            Debug.Log("PlayerFixedMovement.cs >> MoveLeft called.");
-            TryToMovePlayer(-1, 0);
-        }
+        if (!context.performed) return;
+        
+        Debug.Log("PlayerFixedMovement.cs >> MoveLeft called.");
+        TryToMovePlayer(-1, 0);
     }
     
     public void MoveRight(InputAction.CallbackContext context)
     {
         // Ensures that the action is only performed once per key press.
-        if (context.performed)
+        if (!context.performed) return;
+        
+        Debug.Log("PlayerFixedMovement.cs >> MoveRight called.");
+        TryToMovePlayer(1, 0);
+    }
+    
+    // TODO: Clean this method up and make it more efficient. Would like to
+    //       set the Player's position using grid coordinates if possible.
+    
+    public void MoveToStartTile()
+    {
+        if (startTile != null)
         {
-            Debug.Log("PlayerFixedMovement.cs >> MoveRight called.");
-            TryToMovePlayer(1, 0);
+            Vector3 startCoords = startTile.transform.position;
+            Vector3 newPosition = new Vector3(startCoords.x, transform.position.y, startCoords.z);
+            transform.position = newPosition;
+            Debug.Log("PlayerFixedMovement.cs >> Moved player to starting tile.");
+        }
+        else
+        {
+            Debug.LogError("PlayerFixedMovement.cs >> Starting tile is not assigned.");
         }
     }
 
@@ -117,15 +138,40 @@ public class PlayerFixedMovement : MonoBehaviour
     {
         // Calculate the new position on the grid
         int newX = playerGridX + xDir;
-        int newZ = playerGridX + zDir;
+        int newZ = playerGridZ + zDir;
         
-        Debug.Log("PlayerFixedMovement.cs >> Attempting to move the Player to: " + xDir + "," + zDir);
+        Debug.Log($"PlayerFixedMovement.cs >> Attempting to move the Player to: {xDir},{zDir}");
 
         // Check if there's a tile to move to
-        if (!gridManager.IsCellEmpty(newX, newZ))
+        if (gridManager.IsCellEmpty(newX, newZ))
         {
-            Debug.Log($"PlayerFixedMovement.cs >> There is a tile at ({newX},{newZ})");
+            Debug.Log($"PlayerFixedMovement.cs >> There is no tile to jump to at: {newX},{newZ}");
             return;
         }
+        
+        if (!gridManager.IsInsideGrid(newX, newZ))
+        {
+            Debug.Log("PlayerFixedMovement.cs >> Move blocked: Outside grid");
+            return;
+        }
+        
+        gridX = newX;
+        gridZ = newZ;
+
+        // TODO: Add tile type checking here to handle special mechanics.
+        //       The same should be done with a normal tile.
+        // HandleTileType();
+
+        // For right now, we will just snap the player to the new tile.
+        SnapPlayerToTile(gridX, gridZ);
+    }
+
+    public void SnapPlayerToTile(int coordX, int cordZ)
+    {
+        // Grab the X and Z coordinates in Vector3 from the GridManager
+        newCoords = gridManager.GridToWorld(gridX, gridZ);
+        newPosition = new Vector3(newCoords.x, transform.position.y, newCoords.z);
+        transform.position = newPosition;
+        Debug.Log($"Player moved to: {gridX},{gridZ}");
     }
 }
