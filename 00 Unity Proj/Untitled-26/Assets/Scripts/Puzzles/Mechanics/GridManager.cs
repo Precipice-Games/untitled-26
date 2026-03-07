ada4a2275149b1cc4e6cec3fe910175ef1382e6c
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-03-07 16:18:04 -0500

    Updated Comments in GridManager.cs & Updated TileMovement.cs
    Overview
    - Updated doc comments in GridManager.cs.
    - Commented out TileMovement.cs.
    - This commit is related to #156.
    
    In-depth Details
    - Commented out TileMovement.cs since it's not in use at the current moment.
    - If possible, it would be nice to break down the puzzle system logic to get use out of this file.
         - i.e., SelectableTile.cs could be updated to handle only selecting a tile, and TileMovement.cs could be updated to take in that TryMove() method.
         - For time's sake, I will let it be for now
    - Also updated doc comments for clarity in GridManager.cs.

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Mechanics/GridManager.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Mechanics/GridManager.cs
@@ -8,8 +8,8 @@ public class GridManager : MonoBehaviour
     public int height = 3;
     public float tileSize = .25f;
 
+    // Grid of selectable tiles
     private SelectableTile[,] grid;
-
 
     void Awake()
     {
@@ -17,26 +17,57 @@ public class GridManager : MonoBehaviour
         grid = new SelectableTile[width, height];
     }
 
+    /// <summary>
+    /// Check if the movement action is within the grid.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
+    /// <returns></returns>
     public bool IsInsideGrid(int x, int z)
     {
         return x >= 0 && x < width && z >= 0 && z < height;
     }
 
+    /// <summary>
+    /// Check if a cell is vacant.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
+    /// <returns></returns>
     public bool IsCellEmpty(int x, int z)
     {
         return grid[x, z] == null;
     }
 
+    /// <summary>
+    /// Place a tile in the grid at the given coordinates.
+    /// </summary>
+    /// <param name="tile"></param>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
     public void PlaceTile(SelectableTile tile, int x, int z)
     {
         grid[x, z] = tile;
     }
 
+    /// <summary>
+    /// Clear the cell at the given coordinates. This is used when a tile
+    /// moves out of its current cell and into a new one.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
     public void ClearCell(int x, int z)
     {
         grid[x, z] = null;
     }
 
+    /// <summary>
+    /// Converts grid coordinates to world coordinates. This is used to
+    /// position the tiles in the world based on their grid location.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
+    /// <returns></returns>
     public Vector3 GridToWorld(int x, int z)
     {
         return new Vector3(x * tileSize, 0, z * tileSize);
--- 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Mechanics/TileMovement.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Mechanics/TileMovement.cs
@@ -1,36 +1,36 @@
-using UnityEngine;
-
-// TODO: Should probably switch the actual movement process into here.
-//       Right now, SelectableTile.cs is handling both raycasting and
-//       movement, so it would be good if we could break that down.
-
-public class MoveCube : MonoBehaviour
-{
-    public float moveAmount = 0.1f;
-
-    private static MoveCube selectedTile;
-    private Renderer rend;
-    private Color originalColor;
-
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-        originalColor = rend.material.color;
-    }
-
-    void OnMouseDown()
-    {
-        if (selectedTile != null)
-        {
-            selectedTile.ResetColor();
-        }
-
-        selectedTile = this;
-        rend.material.color = Color.yellow;
-    }
-
-    void ResetColor()
-    {
-        rend.material.color = originalColor;
-    }
-}
+// using UnityEngine;
+//
+// // TODO: Should probably switch the actual movement process into here.
+// //       Right now, SelectableTile.cs is handling both raycasting and
+// //       movement, so it would be good if we could break that down.
+//
+// public class MoveCube : MonoBehaviour
+// {
+//     public float moveAmount = 0.1f;
+//
+//     private static MoveCube selectedTile;
+//     private Renderer rend;
+//     private Color originalColor;
+//
+//     void Start()
+//     {
+//         rend = GetComponent<Renderer>();
+//         originalColor = rend.material.color;
+//     }
+//
+//     void OnMouseDown()
+//     {
+//         if (selectedTile != null)
+//         {
+//             selectedTile.ResetColor();
+//         }
+//
+//         selectedTile = this;
+//         rend.material.color = Color.yellow;
+//     }
+//
+//     void ResetColor()
+//     {
+//         rend.material.color = originalColor;
+//     }
+// }
