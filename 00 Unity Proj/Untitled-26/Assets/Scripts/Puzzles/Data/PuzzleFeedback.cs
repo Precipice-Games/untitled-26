4f9f3b7b37cc26966346034165219899c2029946
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-03-28 18:13:29 -0400

    Created a Flicker Timer in PuzzleFeedback.cs
    Overview
    - Updated PuzzleFeedback.cs.
    - This commit is related to #241.
    
    In-depth Details
    - I just created a solid flicker timer that will cause a tile to flash red several times if you try to move it into a spot that is already occupied.
    - This is done by picking up the cellOccupied event from SelectableTile.cs.
    - A timer is then created on the flickerTimer variable, which notifies OnTimerTick() with each tick of the clock.
    - Within OnTimerTick() is a switch-case statement where the color swapping occurs.
    - When the duration is done, we delete it in OnTimerFinished().
    - The only issue is that this works only once on the same tile, so that will need to be sorted out.
    - I think it has something to do with the assignment/deletion of the timer itself.
    - However, this is on the up-and-up!

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Data/PuzzleFeedback.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Data/PuzzleFeedback.cs
@@ -14,7 +14,17 @@ public class PuzzleFeedback : MonoBehaviour
     public GameObject feedbackPrefab;
     public TMP_Text onScreenText;
     
-    private TimerManager.Timer myTimer;
+    private TimerManager.Timer flickerTimer;
+
+    private SelectableTile tile;
+    private Renderer tileRenderer;
+    private Color originalColor;
+    private Color flashColor = Color.red;
+    
+    // Intervals used to determine when to flash the tile's color during the timer
+    private float errorLastIntervalTime = 0f;
+    private float errorInterval = 0.25f;
+    
     
     // Subscribe to events
     private void OnEnable()
@@ -28,27 +38,48 @@ public class PuzzleFeedback : MonoBehaviour
         SelectableTile.cellOccupied -= CellIsOccupied;
     }
 
-    void Start()
+    private void CellIsOccupied(SelectableTile selectableTile)
     {
-        // myTimer = TimerManager.CreateTimer(3.0f, OnTimerFinished, OnTimerTick);
-    }
-
-    private void CellIsOccupied(SelectableTile tile)
-    {
+        // Get the data of the tile before the timer starts so
+        // that we can use it in the tick and finish methods
+        tile = selectableTile;
+        tileRenderer = tile.GetComponent<Renderer>();
+        originalColor = tileRenderer.material.color;
+        
         Debug.Log($"PuzzleFeedback.cs >> You tried to move {tile}, but another tile is occupying that cell!");
-        myTimer = TimerManager.CreateTimer(3.0f, OnTimerFinished, OnTimerTick);
+        flickerTimer = TimerManager.CreateTimer(2.0f, OnTimerFinished, OnTimerTick);
     }
     
     void OnTimerFinished()
     {
         Debug.Log("Timer finished!");
+        tileRenderer.material.color = originalColor;
         
         // MUST delete the timer to prevent a memory leak
-        TimerManager.DeleteTimer(myTimer);
+        TimerManager.DeleteTimer(flickerTimer);
     }
 
     void OnTimerTick()
     {
+        var color = tileRenderer.material.color;
+        float elapsedTime = flickerTimer.GetElapsedTime();
+        bool intervalHit = elapsedTime - errorLastIntervalTime >= errorInterval;
+
+        if (intervalHit)
+        {
+            errorLastIntervalTime = elapsedTime;
+            
+            switch (color)
+            {
+                case var c when c == originalColor:
+                    tileRenderer.material.color = flashColor;
+                    break;
+                case var c when c == flashColor:
+                    tileRenderer.material.color = originalColor;
+                    break;
+            }
+            Debug.Log("Interval hit!");
+        }
         Debug.Log("Timer running...");
     }
 }
