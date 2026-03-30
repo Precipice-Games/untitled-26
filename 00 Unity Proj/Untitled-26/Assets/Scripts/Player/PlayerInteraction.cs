78537ce254ecffca4412ff0e5bb54cec7268d5ca
Cass <cassandra.dawson1@marist.edu>
2026-03-30 19:25:38 -0400

    Check for context.performed
    Overview
    - Ensured that the Interact() method in playerInteraction script is only called once per key press

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Player/PlayerInteraction.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,12 +1,15 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : MonoBehaviour
 {
     public static event Action playerInteraction;
-    public void Interact()
+    public void Interact(InputAction.CallbackContext context)
     {
-        Debug.Log("PlayerInteraction.cs >> Interact() called");
+        if (!context.performed) return;
+
+        //Debug.Log("PlayerInteraction.cs >> Interact() called");
         playerInteraction?.Invoke();
     }
 }
