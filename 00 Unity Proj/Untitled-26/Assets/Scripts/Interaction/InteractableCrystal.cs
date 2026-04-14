8505aab74bcbc4f7d04d9bcbb7dc074de7fb9ae1
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-04-13 23:57:39 -0400

    Added TODO Comment in InteractableCrystal.cs
    Overview
    - Updated InteractableCrystal.cs.
    
    In-depth Details
    - @dbrogen just created this file as a way to transition the Player from Ice Island to Mother Island in 31d11d2.
    - I can confirm this worked on my end and I will be updating the system accordingly to better match the goals outlined in #368.

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractableCrystal.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractableCrystal.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class InteractableCrystal : MonoBehaviour, IInteractable
 {
+    // TODO: Update with proper scene options. Also need to link
+    //       this together with the current island completion
+    //       system. However, I tested it on Ice Island and can
+    //       confirm it sent me to Mother Island. -- Nikki
 
     public void Interaction()
     {
