3188838065058f41b822606b0b0f77219b54aac6
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-04-14 16:43:02 -0400

    Renamed islandCompleted to islandPuzzlesCompleted
    Overview
    - Renamed islandCompleted event to islandPuzzlesCompleted.
    - This commit is related to #372.
    
    In-depth Details
    - The islandCompleted event was originally created when all we needed to go to the next island was to solve the puzzles.
    - Because we're introducing the end crystal, I'm setting up logic to integrate a new event for this that will act similar to the connection between IslandPuzzleManager.cs and IslandManager.cs.
    - I will likely create a new islandCompleted event later on that will be used for the airship itself.

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Data/IslandManager.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Data/IslandManager.cs
@@ -20,7 +20,7 @@ public class IslandManager : MonoBehaviour
 
     [Title("Island Manager Data", "Data regarding the current island.")]
     [PropertyTooltip("Please assign the current island of this scene.")]
-    public IslandName islandName = IslandName.IceIsland;
+    public IslandName islandName = IslandName.IceIsland; // Default is Ice Island
     [PropertyTooltip("Attach the relevant data for this island's puzzles.")]
     public IslandPuzzleManager islandPuzzleManager;
     [PropertyTooltip("Attach the end crystal collectable for this island.")]
@@ -29,32 +29,36 @@ public class IslandManager : MonoBehaviour
     public InMemoryVariableStorage variableStorage;
     
     /// <summary>
-    /// Updates the variable for the current island in the
-    /// Yarn Spinner variable storage.
+    /// Used to verify that all the current island's puzzles have been completed. Also
+    /// updates the variable for the specified island in the YarnSpinner variable storage.
     /// </summary>
-    /// <param name="variableName"></param>
-    /// <param name="value"></param>
-    public void IslandCompleted()
+    public void IslandPuzzlesCompleted()
     {
         switch (islandName)
         {
             case IslandName.MotherIsland:
-                Debug.Log("IslandManager.cs >> Mother Island completed!");
+                Debug.Log("IslandManager.cs >> Mother Island puzzles completed!");
                 // variableStorage.SetValue("motherFinished", true);
                 break;
             case IslandName.IceIsland:
-                Debug.Log("IslandManager.cs >> Ice Island completed!");
+                Debug.Log("IslandManager.cs >> Ice Island puzzles completed!");
                 variableStorage.SetValue("$iceFinished", true);
                 break;
             case IslandName.OasisIsland:
-                Debug.Log("IslandManager.cs >> Oasis Island completed!");
+                Debug.Log("IslandManager.cs >> Oasis Island puzzles completed!");
                 // variableStorage.SetValue("$oasisFinished", true);
                 break;
             
-            // TODO: Add more cases for each island
+            // TODO: Add more cases for each island and check with Matthew about
+            //       the specific variable names in the YarnSpinner variable storage.
         }
     }
 
+    /// <summary>
+    /// Used to verify that the island's end crystal has been collected. This is needed
+    /// for the island itself to be completed in its entirety, and for the Player to be
+    /// able to traverse to other islands using the Airship.
+    /// </summary>
     public void CrystalCollected()
     {
         
