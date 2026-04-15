edd1acfa3dbe6d95f61beb57090ef3b2aeca4971
dbrogen <daniel.brogen1@marist.edu>
2026-04-15 10:49:46 -0400

    Finalized Ice Crystal Behavior

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractableCrystal.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractableCrystal.cs
@@ -3,10 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class InteractableCrystal : MonoBehaviour, IInteractable
 {
+
     // TODO: Update with proper scene options. Also need to link
     //       this together with the current island completion
     //       system. However, I tested it on Ice Island and can
     //       confirm it sent me to Mother Island. -- Nikki
+
+    [SerializeField]
+    PuzzleInformation finalPuzzle;
+
+    private void FixedUpdate()
+    {
+
+        if (finalPuzzle != null && finalPuzzle.puzzleSolved == true && this.transform.position.y < 3.2)
+        {
+            this.transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime, transform.position.z);
+        }
+
+    }
 
     public void Interaction()
     {
