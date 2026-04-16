708c48d2a946f06b6185063ec0c0493aa7720a1c
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-04-16 19:23:06 -0400

    Able to Accelerate & Decelerate with CharacterController
    Overview
    - Updated PlayerMovement.cs.
    - This commit is related to #361.
    
    In-depth Details
    - In 74669df, I expressed that I was able to move the character by pressing WASD, but then the character wouldn't stop.
    - I fixed this now, so the Player is now able to accelerate/decelerate normally.
    - However, input direction has to be corrected because I'm only able to move forward, not in any other direction.
    - Also need to reconfigure the look/turn input handling.

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Player/PlayerMovement.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Player/PlayerMovement.cs
@@ -92,6 +92,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Move();
+        JumpAndGravity();
     }
     
 
@@ -105,10 +106,11 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         // Set the target speed
-        float targetSpeed = moveSpeed;
+        // float targetSpeed = moveSpeed;
+        float targetSpeed = move == Vector2.zero ? 0.0f : moveSpeed;
         
         // Set the target speed to 0 if there is no movement input
-        if (move == Vector2.zero) targetSpeed = 0.0f;
+        // if (move == Vector2.zero) targetSpeed = 0.0f;
         
         // Get the current horizontal speed
         float currentHorizontalSpeed = new Vector3(charController.velocity.x, 0.0f, charController.velocity.z).magnitude;
@@ -129,7 +131,7 @@ public class PlayerMovement : MonoBehaviour
                 Time.deltaTime * SpeedChangeRate);
 
             // round speed to 3 decimal places
-            _speed = Mathf.Round(moveSpeed * 1000f) / 1000f;
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
         }
         else
         {
@@ -140,11 +142,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 inputDirection = new Vector3(xMovement, 0.0f, yMovement).normalized;
         
         // If move input detected, rotate the Player
-        if (move != Vector2.zero)
+        if (look != Vector2.zero)
         {
             targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
             float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref _rotationVelocity, RotationSmoothTime);
-
+            
             // rotate to face input direction relative to camera position
             transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         }
@@ -171,31 +173,16 @@ public class PlayerMovement : MonoBehaviour
         // but this could change if we decide to add sprinting)
         float targetSpeed = _input.move == Vector2.zero ? 0.0f : moveSpeed;
         
-        // Get the current horizontal speed (X, Z)
-        float currentHorizontalSpeed = new Vector3(charController.velocity.x, 0.0f, charController.velocity.z).magnitude;
+        // // Normalize look input direction
+        // Vector3 inputDirection = new Vector3(look.x, 0.0f, look.y).normalized;
+        //
+        // targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+        // float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref _rotationVelocity, RotationSmoothTime);
+        //
+        // // rotate to face input direction relative to camera position
+        // transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         
-        // create a float input magnitude
-        // float inputMagnitude = Mathf.Clamp01(_input.move.magnitude);
-        float inputMagnitude = 1f;
-        
-        // Accelerate or decelerate to target speed
-        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-            currentHorizontalSpeed > targetSpeed + speedOffset)
-        {
-            // Use Lerp() to smoothly interpolate between the current speed and the
-            // target speed, based on the input magnitude and the speed change rate.
-            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
-            
-            // Round speed to reduce jitter
-            _speed = Mathf.Round(_speed * 1000f) / 1000f;
-        }
-        else
-        {
-            _speed = targetSpeed;
-        }
-        
-        // normalise input direction
-        inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+    }
 
     /// <summary>
     /// Takes the player's jump input in the context parameter
@@ -206,9 +193,20 @@ public class PlayerMovement : MonoBehaviour
     /// <param name="context"></param>
     public void PlayerJump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        // if (context.performed && isGrounded)
+        // {
+        //     Debug.Log("PlayerMovement.cs >> Jump performed.");
+        // }
+
+        if (context.performed)
         {
+            jump = true;
             Debug.Log("PlayerMovement.cs >> Jump performed.");
+        }
+        else if (context.canceled)
+        {
+            jump = false;
+            Debug.Log("PlayerMovement.cs >> Jump canceled.");
         }
         
         // Normally, we would run the following:
