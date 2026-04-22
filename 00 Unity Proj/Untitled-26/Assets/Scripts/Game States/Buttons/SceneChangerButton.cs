cb254f1b853505eebea91a057aef3d6ecc38756b
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2026-04-22 10:07:44 -0400

    Updated SceneChangerButton.cs
    Overview
    - Updated SceneChangerButton.cs.
    - This commit is related to #404.
    
    In-depth Details
    - I just improved the SceneChangerButton.cs script, which is attached to the "Start" button in the Main Menu.
    - Previously, it just took in a string that had to be entered in the inspector.
         - I converted this to be an Enum named SceneDestination.
    - When we click the button, we call LoadScene(), which creates a local variable based on the DetermineScene() method, which is set to return a string.
    - LoadScene() uses that string to load up the next scene.

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Game States/Buttons/SceneChangerButton.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Game States/Buttons/SceneChangerButton.cs
@@ -1,12 +1,64 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// This script is used for buttons that change the scene when pressed.
+// It contains an enum for a safer way to determine the next scene, as
+// opposed to just using a string.
+
 public class SceneChangerButton :  MonoBehaviour
 {
-    public string scene = "<Insert scene name>";
+    public enum SceneDestination
+    {
+        MainMenu,
+        MotherIsland,
+        IceIsland,
+        OasisIsland
+        
+        // Add flower island later
+    }
+    
+    [Title("SceneChanger Variables", "Variables related to scene switching.")]
+    [PropertyTooltip("The scene to load upon pressing this button.")]
+    public SceneDestination nextDestination = SceneDestination.MotherIsland; // Default is Mother Island
+    
+    // Placeholder string that will store name of next scene
+    private string scene = "scene";
 
+    /// <summary>
+    /// Called by LoadScene(). Returns a string that is the name of the
+    /// next scene to load, based on the value of nextDestination.
+    /// </summary>
+    /// <param name="destination"></param>
+    /// <returns></returns>
+    private string DetermineScene(SceneDestination destination)
+    {
+        switch (destination)
+        {
+            case SceneDestination.MainMenu:
+                scene = "MainMenu";
+                break;
+            case SceneDestination.MotherIsland:
+                scene = "Mother_Island";
+                break;
+            case SceneDestination.IceIsland:
+                scene = "Ice_Island";
+                break;
+            case SceneDestination.OasisIsland:
+                scene = "Oasis_Island";
+                break;
+        }
+        
+        return scene;
+    }
+    
+    /// <summary>
+    /// Calls DetermineScene() to get the name of the next scene to load.
+    /// Triggered by the button's OnClick() event in the Inspector.
+    /// </summary>
     public void LoadScene()
     {
-        SceneManager.LoadScene(scene);
+        var nextScene = DetermineScene(nextDestination);
+        SceneManager.LoadScene(nextScene);
     }
 }
