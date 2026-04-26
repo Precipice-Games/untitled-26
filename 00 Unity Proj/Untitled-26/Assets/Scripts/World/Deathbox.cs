ee28aabd520f72aa342f9262868da7730c575016
Cass <cassandra.dawson1@marist.edu>
2026-04-26 16:44:05 -0400

    Reorganized Mother_Island Hierarchy
    Overview
    - Reorganized mother island scene hierarchy to match ice island's organization
    - Created a deathbox prefab and added it to mother island scene

--- 00 Unity Proj/Untitled-26/Assets/Scripts/World/Deathbox.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/World/Deathbox.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 public class Deathbox : MonoBehaviour
 {
     public Transform[] respawnLocations;
-    public IslandManager islandManager;
 
     [SerializeField] private int puzzlesComplete = 0;
 
+    // Set respawn location to next respawn point, based on number of puzzles completed.
     public void OnPuzzleCompletion()
     {
         puzzlesComplete++;
     }
 
+    // Reset respawn location to the beginning of the island when the crystal is collected,
+    // as the player will be teleported back to the beginning of the island after collecting the crystal.
     public void OnCrystalCollect()
     {
         puzzlesComplete = 0;
     }
 
+    // Teleport player to respawn location when they enter the deathbox trigger.
+    // The respawn location is determined by the number of puzzles completed, which is updated by the OnPuzzleCompletion().
     private void OnTriggerEnter(Collider other)
     {
         
